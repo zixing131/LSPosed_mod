be2342194c6c3faabbd259ba67a69aaa9b12c6df
Keeperorowner <qingfong0731@outlook.com>
2024-10-31 22:02:28 +0800

    Add action.sh to open LSPosed manager (#74)
    * Update customize.sh
    
    * Create action.sh
    
    * Keep WebUI as fallback
    
    ---------
    
    Co-authored-by: JingMatrix <jingmatrix@gmail.com>

---
+++ magisk-loader/magisk_module/action.sh
@@ -0,0 +1,4 @@
+DEFAULT_MANAGER_PACKAGE_NAME=org.lsposed.manager
+INJECTED_PACKAGE_NAME=com.android.shell
+
+am start -c "${DEFAULT_MANAGER_PACKAGE_NAME}.LAUNCH_MANAGER" "${INJECTED_PACKAGE_NAME}/.BugreportWarningActivity"
--- magisk-loader/magisk_module/customize.sh
+++ magisk-loader/magisk_module/customize.sh
@@ -72,6 +72,7 @@ fi
 ui_print "- Extracting module files"
 
 extract "$ZIPFILE" 'module.prop'        "$MODPATH"
+extract "$ZIPFILE" 'action.sh'          "$MODPATH"
 extract "$ZIPFILE" 'post-fs-data.sh'    "$MODPATH"
 extract "$ZIPFILE" 'service.sh'         "$MODPATH"
 extract "$ZIPFILE" 'uninstall.sh'       "$MODPATH"
