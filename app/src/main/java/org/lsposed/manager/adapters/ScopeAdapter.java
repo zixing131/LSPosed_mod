8f1ae22905fd0afca174947252931a60aef0a24d
JingMatrix <jingmatrix@gmail.com>
2025-03-28 09:53:05 +0100

    Deselect all apps when module scope is empty
    UI should reflect correctly current module scope.
    Fix the problem that after clicking on menu_select_none, recommended apps are still checked even though the scope database is already updated.
    
    This bug was introduced due to the inconsistent designs in https://github.com/JingMatrix/LSPosed/commit/1339fc7d3caa45eba31537289035619223c96a28 and https://github.com/JingMatrix/LSPosed/commit/7be4e66b989978d4f75a3bac27a43c8a059738c5.

--- app/src/main/java/org/lsposed/manager/adapters/ScopeAdapter.java
+++ app/src/main/java/org/lsposed/manager/adapters/ScopeAdapter.java
@@ -557,11 +557,6 @@ public class ScopeAdapter extends EmptyStateRecyclerView.EmptyStateAdapter<Scope
                     synchronized (tmpRecList) {
                         tmpRecList.add(application);
                     }
-                    if (emptyCheckedList) {
-                        synchronized (tmpChkList) {
-                            tmpChkList.add(application);
-                        }
-                    }
                 } else if (shouldHideApp(info, application, tmpChkList)) {
                     return;
                 }
