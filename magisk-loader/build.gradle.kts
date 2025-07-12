b9f091ab251a9fd5441636805dc1403880b39e21
JingMatrix <jingmatrix@gmail.com>
2025-07-13 00:27:53 +0200

    Fix the author name

--- magisk-loader/build.gradle.kts
+++ magisk-loader/build.gradle.kts
@@ -30,7 +30,7 @@ plugins {
 
 val moduleName = "LSPosed"
 val moduleBaseId = "lsposed"
-val authors = "Jing Matrix & LSPosed Developers"
+val authors = "JingMatrix & LSPosed Developers"
 
 val injectedPackageName: String by rootProject.extra
 val injectedPackageUid: Int by rootProject.extra
